a642718418c9d2b8d4c6bc28dd3702e31a5e12b6
Code-ever <166537703+code-ever@users.noreply.github.com>
2025-04-21 23:31:14 +0100

    commit database config

--- db/connetDB.js
+++ db/connetDB.js
@@ -1,10 +1,10 @@
 const mysql2 = require('mysql2')
 
 const db = mysql2.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'stLuke'
+    host: process.env.HOST_NAME,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME
 }).promise();
 
 module.exports = db
